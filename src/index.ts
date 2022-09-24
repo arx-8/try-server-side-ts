cd9e37d2a857da9207c1b1763c01adaa4b4420d4
arx-8 <swans.randomness@gmail.com>
2022-09-24 19:13:05 +0900

    feat: run on docker

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,26 @@ const app = Fastify({
 
 app.get("/", indexGet)
 
-const port = 3000
-app.listen({ port }, (err, address) => {
+/**
+ * Need to explicitly give host for Docker
+ * @see https://github.com/fastify/fastify/issues/935#issuecomment-388569718
+ */
+const host = "0.0.0.0"
+const port = 30001
+app.listen({ port, host }, (err, address) => {
   if (err) throw err
-  console.log(`Server is now listening on ${address}!`)
+  console.log(`⚡️ Server is now listening on ${address} !`)
 })
+
+/**
+ * For Docker graceful shutdown
+ */
+const shutdownGraceful: NodeJS.SignalsListener = (signal) => {
+  console.log(`⚡️ Received signal to terminate: ${signal}`)
+  app.close().then(() => {
+    console.log("⚡️ Server terminated gracefully.")
+    process.exit()
+  })
+}
+process.on("SIGINT", shutdownGraceful)
+process.on("SIGTERM", shutdownGraceful)
